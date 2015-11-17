39362c811c8ebdeb80134a45507d6ff2b7dd0c67
Angel Amaya <birohamaruso007@gmail.com>
2015-11-17 12:17:45 +0000

    Initial Commit

---
+++ Velocimetro--Coursera/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Velocimetro--Coursera
+//
+//  Created by Angel Amaya on 17/11/15.
+//  Copyright © 2015 Angel. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
